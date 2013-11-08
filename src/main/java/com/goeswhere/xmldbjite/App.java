905e939bbbc41f40fb8ea12cbd40bcffe04e315b
Chris West (Faux) <git@goeswhere.com>
2013-11-08 18:20:59 +0000

    archetype

---
+++ src/main/java/com/goeswhere/xmldbjite/App.java
@@ -0,0 +1,13 @@
+package com.goeswhere.xmldbjite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
